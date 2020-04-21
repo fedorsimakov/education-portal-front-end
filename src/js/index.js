612c886e4b26bc6ce943481bc09d5a377b959d68
fedorsimakov <fedor.simakov.a@gmail.com>
2020-04-21 11:49:05 +0500

    config bootstrap

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,28 @@
 import jQuery from 'jquery';
 import popper from 'popper.js';
+
+// **************************************
+// * Option A: Include all of Bootstrap *
+// **************************************
+
 import bootstrap from 'bootstrap';
 
-jQuery(() => {
-  alert ('Privet!');
-  jQuery('body').css('background-color', 'blue');
-});
+// ****************************************
+// * Option B: Include parts of Bootstrap *
+// ****************************************
+
+// import 'bootstrap/js/dist/alert';
+// import 'bootstrap/js/dist/button';
+// import 'bootstrap/js/dist/carousel';
+// import 'bootstrap/js/dist/collapse';
+// import 'bootstrap/js/dist/dropdown';
+// import 'bootstrap/js/dist/index';
+// import 'bootstrap/js/dist/modal';
+// import 'bootstrap/js/dist/popover';
+// import 'bootstrap/js/dist/scrollspy';
+// import 'bootstrap/js/dist/tab';
+// import 'bootstrap/js/dist/toast';
+// import 'bootstrap/js/dist/tooltip';
+// import 'bootstrap/js/dist/util';
 
 console.log('Hello, world');
